31d85382dbb1c293523b633138bb049d343fd800
Isaac Slavitt <isaac.slavitt@gmail.com>
2021-08-03 21:42:37 -0400

    Standardize names

--- runtime/tests/test_submission.py
+++ runtime/tests/test_submission.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from skimage.io import imread
 
 SUBMISSION_DIR = Path("submission")
-INPUT_IMAGES_DIRECTORY = Path("data/test_features")
+INPUT_IMAGES_DIR = Path("data/test_features")
 MAX_FILE_SIZE = 512 * 512 * 2  # 2x fudge factor over one byte (uint8) per pixel
 
 
@@ -11,7 +11,7 @@ def get_expected_image_ids():
     """
     Use the submission format directory to see which images are expected
     """
-    paths = INPUT_IMAGES_DIRECTORY.glob("*.tif")
+    paths = INPUT_IMAGES_DIR.glob("*.tif")
     # images are named something like abc12.tif, we only want the abc12 part
     ids = list(sorted(set(path.stem.split("_")[0] for path in paths)))
     return ids
@@ -23,7 +23,7 @@ def test_all_files_in_format_have_corresponding_submission_file():
         submission_path = SUBMISSION_DIR / filename
         assert submission_path.exists(), f"File {filename} missing from your submission"
 
-        input_path = INPUT_IMAGES_DIRECTORY / f"{image_id}_vv.tif"
+        input_path = INPUT_IMAGES_DIR / f"{image_id}_vv.tif"
         input_arr = imread(input_path)
         output_arr = imread(submission_path)
         expected_shape = input_arr.shape
@@ -39,7 +39,7 @@ def test_no_unexpected_tif_files_in_submission():
     for submission_path in SUBMISSION_DIR.glob("*.tif"):
         filename = submission_path.name
         image_id = filename.split(".")[0]
-        input_path = INPUT_IMAGES_DIRECTORY / f"{image_id}_vv.tif"
+        input_path = INPUT_IMAGES_DIR / f"{image_id}_vv.tif"
         assert input_path.exists(), f"Found unexpected file {filename} in submission"
 
 
